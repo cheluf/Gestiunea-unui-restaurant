de74e4f06bad02cf35da141ed17defd127170b29
cheluf <cheluf26@gmail.com>
2021-04-28 13:03:37 +0300

    Etapa 2

--- src/com/company/audit.java
+++ src/com/company/audit.java
@@ -7,16 +7,16 @@ import java.io.PrintWriter;
 import java.util.Date;
 
 public class audit {
-    public static void saudit (String actionName) {
-        String path = "src/com/company/CSV/audit.csv";
+    public static void saudit (String actiune) {
+        String locatie = "src/com/company/CSV/audit.csv";
         try {
-            FileWriter fileWriter = new FileWriter(path, true);
+            FileWriter fileWriter = new FileWriter(locatie, true);
             BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
             PrintWriter printWriter = new PrintWriter(bufferedWriter);
 
             Date date = new Date();
 
-            printWriter.println(actionName + "," + date.toString());
+            printWriter.println(actiune + "," + date.toString());
             printWriter.flush();
             printWriter.close();
         } catch (IOException e) {
